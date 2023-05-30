995c3dc3e8bbfdd358750de3cc559068c3418d14
George Owen <gbo.owen@gmail.com>
2023-05-29 22:00:36 -0700

    image zoom output stuff

--- image-generator/create-zoomable-image.js
+++ image-generator/create-zoomable-image.js
@@ -3,11 +3,11 @@ import sharp from "sharp";
 const input = './public/wall-images/out.tiff';
 
 sharp(input)
-  .png()
+  .jpeg()
   .tile({
     size: 512
   })
-  .toFile('./public/wall-images/output.dz', function(err, info) {
+  .toFile('./public/wall-images/test-25.dz', function(err, info) {
     console.log(err);
     console.log(info);
     // output.dzi is the Deep Zoom XML definition
---
+++ public/wall-images/README.md
@@ -0,0 +1,1 @@
+this folder contains the output of image files
